1bba0ecb8eb91f972a4c6e15f335b2b5d598a775
Xin Jiang <jiangxinxin0120@gmail.com>
2024-02-07 19:36:23 -0500

    Fix issue #1: Updated TodoItems data

--- app2demoissue/Program.cs
+++ app2demoissue/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace app2demoissue
+﻿using System;
+using System.Collections.Generic;
+
+namespace app2demoissue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            
+            List<string> todoItems = new List<string>
+            {
+                "quiz#2",
+                "Update project documentation",
+                "Review pull requests"
+            };
+
+            Console.WriteLine("Todo Items:");
+            foreach (var item in todoItems)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
