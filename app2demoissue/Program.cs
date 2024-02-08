bce28f09dc8f2e8cbbc9810ff50248027f7ea904
KalelDev <akhalee1@my.centennialcollege.ca>
2024-02-07 19:56:23 -0500

    Fixes #1: Add quiz#2 to TodoItems

--- app2demoissue/Program.cs
+++ app2demoissue/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace app2demoissue
+﻿using System;
+using System.Collections.Generic;
+
+namespace app2demoissue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> todoItems = new List<string>
+            {
+                "Complete assignment",
+                "Read chapter 4"
+            };
+
+            Console.WriteLine("Initial Todo Items:");
+            foreach (var item in todoItems)
+            {
+                Console.WriteLine(item);
+            }
+
+            todoItems.Add("quiz#2");
+
+            Console.WriteLine("\nUpdated Todo Items:");
+            foreach (var item in todoItems)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
